4a8fcef51ad8a8661b8b368439f4128baf651c51
Neal JIn Oh <no4259@gmail.com>
2019-10-28 17:27:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(name, event){
+  let arr = [];
+  for (let i = 0; i < name.length; i += 1){
+    arr.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+  }
+  return arr;
+}
+
+
+function countDown(num) {
+  let i = 10;
+  while( 0 <= i ){
+    console.log(i);
+    i -= 1
+  }
+}
+countDown(10);
